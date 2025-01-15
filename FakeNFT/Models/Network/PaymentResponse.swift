b66e6a471ccdd0e3017eed8f51ab4e0c698864e9
Doroteya Galbacheva <galbachevva@gmail.com>
2025-01-15 11:45:06 +0300

    Final Epic Catalog

---
+++ FakeNFT/Models/Network/PaymentResponse.swift
@@ -0,0 +1,14 @@
+//
+//  Payment.swift
+//  FakeNFT
+//
+//  Created by Doroteya Galbacheva on 14.01.2025.
+//
+
+import Foundation
+
+struct Payment: Codable {
+    let success: Bool
+    let orderId: String
+    let id: String
+}
---
+++ FakeNFT/Services/ProfileService.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileService.swift
+//  FakeNFT
+//
+//  Created by MacBook Pro 15 on 14.01.2025.
+//
+
+import Foundation
